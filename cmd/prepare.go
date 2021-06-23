e176f0307d472f8613dbb46b00c0108ba1b835ae
Jack Warren <jack@jackwarren.info>
2021-06-23 16:33:40 -0400

    nits

--- cmd/prepare.go
+++ cmd/prepare.go
@@ -1,36 +1,30 @@
 package cmd
 
 import (
-    "fmt"
+	"fmt"
+	"github.com/spf13/viper"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 // prepareCmd represents the prepare command
 var prepareCmd = &cobra.Command{
-    Use:   "prepare",
-    Short: "A brief description of your command",
-    Long: `A longer description that spans multiple lines and likely contains examples
+	Use:   "prepare",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("prepare called")
-    },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("prepare called")
+		if viper.GetBool("verbose") {
+			fmt.Println(viper.GetString("statuspage.apiKey"))
+		}
+	},
 }
 
 func init() {
-    rootCmd.AddCommand(prepareCmd)
-
-    // Here you will define your flags and configuration settings.
-
-    // Cobra supports Persistent Flags which will work for this command
-    // and all subcommands, e.g.:
-    // prepareCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-    // Cobra supports local flags which will only run when this command
-    // is called directly, e.g.:
-    // prepareCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(prepareCmd)
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,21 @@
 package cmd
 
 import (
-    "fmt"
-    "github.com/spf13/cobra"
-    "os"
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"strings"
 
-    "github.com/spf13/viper"
+	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-    Use:   "terra-status-manager",
-    Short: "Interact with Terra's production Statuspage",
-    Long: `Requires a configuration file via --config, ./terra-status-manager.yaml,
+	Use:   "terra-status-manager",
+	Short: "Interact with Terra's production Statuspage",
+	Long: `Requires a configuration file via --config, ./terra-status-manager.yaml,
 or /etc/terra-status-manager/terra-status-manager.yaml.
 
 To configure Statuspage.io based on the config file:
@@ -23,40 +24,42 @@ To configure Statuspage.io based on the config file:
 
 // Execute adds all child commands to the root command, only necessary for rootCmd
 func Execute() {
-    cobra.CheckErr(rootCmd.Execute())
+	cobra.CheckErr(rootCmd.Execute())
 }
 
 // init configures flags (both persistent across child commands and local to root)
 func init() {
-    cobra.OnInitialize(initConfig)
-    rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./terra-status-manager.yaml)")
-    rootCmd.PersistentFlags().BoolP("verbose", "v", false, "enable more verbose console output")
-    err := viper.BindPFlags(rootCmd.Flags())
-    cobra.CheckErr(err)
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./terra-status-manager.yaml)")
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "enable more verbose console output")
+	err := viper.BindPFlags(rootCmd.Flags())
+	cobra.CheckErr(err)
 }
 
 // initConfig reads in config file (flag or default) and ENV variables if set
 func initConfig() {
-    if cfgFile != "" {
-        viper.SetConfigFile(cfgFile)
-    } else {
-        viper.AddConfigPath(".")
-        viper.AddConfigPath("/etc/terra-status-manager/")
-        viper.SetConfigName("terra-status-manager")
-    }
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath(".")
+		viper.AddConfigPath("/etc/terra-status-manager/")
+		viper.SetConfigName("terra-status-manager")
+	}
 
-    viper.SetEnvPrefix("TSM")
-    viper.AutomaticEnv()
+	// example: statuspage.apiKey overridden by env var TSM_STATUSPAGE_APIKEY
+	viper.SetEnvPrefix("TSM")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 
-    err := viper.ReadInConfig()
-    if err != nil {
-        if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-            _, err := fmt.Fprintln(os.Stderr, "Not using a configuration file!")
-            cobra.CheckErr(err)
-        }
-        cobra.CheckErr(err)
-    } else {
-        _, err := fmt.Println("Using configuration file:", viper.ConfigFileUsed())
-        cobra.CheckErr(err)
-    }
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			_, err := fmt.Fprintln(os.Stderr, "Not using a configuration file!")
+			cobra.CheckErr(err)
+		}
+		cobra.CheckErr(err)
+	} else {
+		_, err := fmt.Println("Using configuration file:", viper.ConfigFileUsed())
+		cobra.CheckErr(err)
+	}
 }
