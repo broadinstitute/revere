ac1e063846b7abddbead2ec03cecc334ad791ae2
Jack Warren <jack@jackwarren.info>
2021-08-30 17:55:29 -0400

    make serve do both api and messages

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,13 +1,22 @@
 package cmd
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/broadinstitute/revere/internal/api"
 	"github.com/broadinstitute/revere/internal/configuration"
 	"github.com/broadinstitute/revere/internal/pubsub"
 	"github.com/broadinstitute/revere/internal/pubsub/pubsubapi"
 	"github.com/broadinstitute/revere/internal/shared"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 )
 
 var serveCmd = &cobra.Command{
@@ -24,18 +33,71 @@ Input event sources:
 func Serve(*cobra.Command, []string) {
 	config, err := configuration.AssembleConfig(viper.GetViper())
 	cobra.CheckErr(err)
-	client, err := pubsubapi.Client(config)
-	cobra.CheckErr(err)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		shared.LogLn(config, "listening to pubsub...")
-		err = pubsub.ReceiveMessages(config, client)
-		cobra.CheckErr(err)
-		wg.Done()
-	}()
-	wg.Wait()
+	pubsubClient, err := pubsubapi.Client(config)
+	cobra.CheckErr(err)
+	pubsubCtx, cancelPubsub := context.WithCancel(context.Background())
+
+	apiServer := &http.Server{
+		Addr:    fmt.Sprintf(":%d", config.Api.Port),
+		Handler: api.NewRouter(config),
+	}
+
+	routines := []struct {
+		runForever   func()
+		uponShutdown func()
+	}{
+		{
+			runForever: func() {
+				shared.LogLn(config, "listening to pubsub...")
+				err := pubsub.ReceiveMessages(config, pubsubClient, pubsubCtx)
+				cobra.CheckErr(err)
+			},
+			uponShutdown: func() {
+				cancelPubsub()
+			},
+		},
+		{
+			runForever: func() {
+				shared.LogLn(config, fmt.Sprintf("serving api on port %d...", config.Api.Port))
+				if err := apiServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+					cobra.CheckErr(err)
+				}
+			},
+			uponShutdown: func() {
+				apiShutdownCtx, apiShutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer apiShutdownCancel()
+				err := apiServer.Shutdown(apiShutdownCtx)
+				cobra.CheckErr(err)
+			},
+		},
+	}
+
+	// Run continuous routines forever
+	for _, routine := range routines {
+		go routine.runForever()
+	}
+
+	// Block waiting for SIGINT/SIGTERM
+	// We can't capture SIGKILL so no need to include
+	shutdownChannel := make(chan os.Signal)
+	signal.Notify(shutdownChannel, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdownChannel
+
+	// Run shutdown routines "forever", use waitgroup to synchronize
+	// (
+	shared.LogLn(config, "shutting down...")
+	var shutdownWaitGroup sync.WaitGroup
+	for _, routine := range routines {
+		shutdownWaitGroup.Add(1)
+		// Intermediate variable to lock in routine referenced in func
+		r := routine
+		go func() {
+			defer shutdownWaitGroup.Done()
+			r.uponShutdown()
+		}()
+	}
+	shutdownWaitGroup.Wait()
 }
 
 func init() {
