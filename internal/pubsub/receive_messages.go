73c174608ffce719258d44639ce30c6614edf5fc
Jack Warren <jack@jackwarren.info>
2021-09-16 14:53:15 -0400

    print the labels

--- internal/pubsub/receive_messages.go
+++ internal/pubsub/receive_messages.go
@@ -3,14 +3,27 @@ package pubsub
 import (
 	"cloud.google.com/go/pubsub"
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/broadinstitute/revere/internal/cloudmonitoring"
 	"github.com/broadinstitute/revere/internal/configuration"
+	"github.com/broadinstitute/revere/internal/shared"
 )
 
 // receiveOnce should handle and acknowledge a single message; will run
 // asynchronously
 func receiveOnce(config *configuration.Config, msg *pubsub.Message) {
-	fmt.Printf("Message %q from %s", string(msg.Data), config.Pubsub.SubscriptionID)
+	var packet *cloudmonitoring.MonitoringPacket
+	if err := json.Unmarshal(msg.Data, &packet); err != nil {
+		shared.LogLn(config, "failed to parse packet", fmt.Sprintf("%+v", err))
+		return
+	}
+	labels, err := packet.ParseLabels()
+	if err != nil {
+		shared.LogLn(config, "failed to parse labels", fmt.Sprintf("%+v", err))
+		return
+	}
+	fmt.Printf("Alert %+v from %s", labels, config.Pubsub.SubscriptionID)
 	msg.Ack()
 }
 
